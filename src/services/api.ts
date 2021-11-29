289c14fbf5aea64517f76049542c24bfd6db0561
Pedro Campos <pedro@cdias.dev>
2021-11-29 18:27:43 -0300

    Update api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://playground-backend-mock.herokuapp.com',
+  baseURL:
+    /* 'https://playground-backend-mock.herokuapp.com' */ 'http://445d-191-223-168-78.ngrok.io',
 });
 
 export { api };
