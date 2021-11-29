440974a803e947bc109e5a3e3fb6f27c5fbb886f
Pedro Campos <pedro@cdias.dev>
2021-11-29 18:32:02 -0300

    Fix url

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL:
-    /* 'https://playground-backend-mock.herokuapp.com' */ 'http://445d-191-223-168-78.ngrok.io',
+    /* 'https://playground-backend-mock.herokuapp.com' */ 'https://445d-191-223-168-78.ngrok.io',
 });
 
 export { api };
